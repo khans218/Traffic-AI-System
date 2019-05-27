0979f6d26189a002b38108795a794941530d32d7
khans218 <salarqkhan50@gmail.com>
2019-05-27 02:36:06 -0400

    added a horn script

---
+++ Assets/horn.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class horn : MonoBehaviour {
+    AIVehicle target;
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKey("h"))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.forward, out hit, 8.0f))
+            {
+                Transform other = hit.collider.transform;
+                while (other.parent != null)
+                {
+                    other = other.parent;
+                }
+                if (other.name == "AIVehicle")
+                {
+                    target = other.GetComponent<AIVehicle>();
+                    target.moveAside = true;
+                }
+            } else
+            {
+                if (target != null)
+                {
+                    target.moveAside = false;
+                }
+            }
+        } else
+        {
+            if (target != null)
+            {
+                target.moveAside = false;
+            }
+        }
+	}
+}
